418176615b8660d922cf0d303415123c9a7397d3
Tandi Samples <tandisamples@gmail.com>
2019-12-02 17:50:50 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,7 +2,7 @@ class Student
   attr_accessor :id, :name, :grade
 
   def self.new_from_db(row)
-    # create a new Student object given a row from the database
+    new_student = self.new_studentnew_studentid = row[0]
   end
 
   def self.all
