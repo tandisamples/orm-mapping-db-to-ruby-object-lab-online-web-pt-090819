1711bd0595e6b90debf3dd3b21bf38833711bbe8
Tandi Samples <tandisamples@gmail.com>
2019-12-02 17:59:51 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -25,8 +25,7 @@ class Student
   end
 
   def self.find_by_name(name)
-    
-    sql= <<-SQL
+      sql= <<-SQL
       SELECT * FROM students
       WHERE name = ?
       LIMIT l
@@ -36,8 +35,7 @@ class Student
       self.new_from_db(row)
     end.first
     
-    
-    # find the student in the database given a name
+        # find the student in the database given a name
     # return a new instance of the Student class
   end
   
